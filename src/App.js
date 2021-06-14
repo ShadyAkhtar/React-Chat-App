59ff2c26494f2a8e03a529fac2e225b8576caed4
Shadab <akhtarshadab415@gmail.com>
2021-06-14 22:04:28 +0530

    React Chat App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+
+const projectID = '8d1e2960-4b4d-4ec2-bc4d-46b72642d1ac';
 
 const App = () => {
+    if (!localStorage.getItem('username')) return <LoginForm />;
+    
     return (
         <ChatEngine
         height="100vh"
-        projectID="8d1e2960-4b4d-4ec2-bc4d-46b72642d1ac"
-        userName="Shadab"
-        userSecret="rMkqqY$YZ6E$"
+        projectID={projectID}
+        userName={localStorage.getItem('username')}
+        userSecret={localStorage.getItem('password')}
+        renderChatFeed={(chatAppProps) => <ChatFeed { ...chatAppProps} />}
+        onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
         />
     );
 }
